c5ea9207bcfab8b5f328e01330c25467b47568c9
changtengfei <tengfei.chang@gmail.com>
2014-10-15 11:51:34 +0800

    Initial the project.

---
+++ serialport.py
@@ -0,0 +1,38 @@
+import _winreg as winreg
+import itertools
+
+class serial_Ports():
+    # list contains all port device info
+    portList = []
+
+    def enumerate_serial_ports(self):
+        """ Uses the Win32 registry to return a iterator of serial 
+            (COM) ports existing on this computer.
+        """
+        path = 'HARDWARE\\DEVICEMAP\\SERIALCOMM'
+        try:
+            key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, path)
+        except WindowsError:
+            raise IterationError
+
+        for i in itertools.count():
+            try:
+                val = winreg.EnumValue(key, i)
+                self.portList.append(val)
+            except EnvironmentError:
+                break
+
+        winreg.CloseKey(key)
+
+    def port_list(self):
+        for x in range(len(self.portList)):
+            tmp = self.portList[x]
+            print tmp[0]
+            print str(tmp[1])
+
+if __name__ == "__main__":
+    
+    sPort = serial_Ports()
+    
+    sPort.enumerate_serial_ports()
+    sPort.port_list()
